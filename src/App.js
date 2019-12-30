473ec0b9abcec079e6052c6fb69840c0f586b5ad
Brijesh Dhanani <Brijesh Dhanani>
2019-12-30 10:41:27 +0530

    enhancement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Fragment } from "react";
 import axios from "axios";
-import ExamplePage from "./component/content";
 import "./App.css";
 import {
     BrowserRouter as Router,
-    Route
 } from "react-router-dom";
 
 export default class App extends React.Component {
@@ -12,49 +10,43 @@ export default class App extends React.Component {
         super(props);
 
         this.state = {
-            pages: [],
             posts: []
         };
     }
 
-    getAllPages = async () => {
-        let res = await axios.get(
-            'http://localhost/gutenberg-demo/wp-json/sections/v1/todo-list'
-        );
-        let { data } = await res;
-
-        this.setState({ pages: data });
-    };
-
     getAllPosts = async () => {
-      let res = await axios.get(
+      let responce = await axios.get(
         'http://localhost/gutenberg-demo/wp-json/sections/v1/todo-list'
       );
-        let { data } = await res;
+        let { data } = await responce;
         this.setState({ posts: data });
     };
 
     componentDidMount = async () => {
-      await this.getAllPages();
       await this.getAllPosts();
     };
 
     render() {
-        const { pages,posts } = this.state;
+        const { posts } = this.state;
 
         return (
             <Router>
               <Fragment>
                     {/* Links */}
-                    <div class="container">
-                        {pages.map((page, index) => {
+                    <div className="container">
+                      <div className="wrap-list">
+                        {posts.map((page, index) => {
                             return (
                               <>
-                                <h2 class={"post-title"}>{page.post_title}</h2>
-                                <div class={"post-content"}>{page.post_content}</div>
+                              <div className="wrap-input">
+                                <input type="checkbox" name={page.post_title} value={page.post_title} />
+                                <h2 className={"post-title"}>{page.post_title}</h2>
+                                <div className={"post-content"}>{page.post_content}</div>
+                              </div>
                               </>
                             );
                         })}
+                      </div>
                     </div>
               </Fragment>
             </Router>
